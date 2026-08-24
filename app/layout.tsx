d47a6f6406041c698e0cda568de87e220aecf6fb
javad136999 <84092351+javad136999@users.noreply.github.com>
2026-08-24 06:32:24 +0330

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import BottomNav from "@/components/BottomNav";
 import VisitTracker from "@/components/VisitTracker";
 
 export const metadata: Metadata = {
-  title: "جم‌سیتی | همه‌چیز برای زندگی بهتر در جم",
+  title: "35770718",
   description:
     "جم‌سیتی؛ پلتفرم شهری جم برای ثبت آگهی، پیدا کردن کسب‌وکارها و خدمات شهری و گفتگو با شهروندان جم.",
   manifest: "/manifest.json",
